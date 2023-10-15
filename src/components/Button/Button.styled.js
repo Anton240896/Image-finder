b2d0177e60858dcfc884b6daae6e0015145422d2
Anton240896 <antonalipov791@gmail.com>
2023-10-15 21:04:06 +0300

    Update Button.styled.js

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const ButtonLoadMore = styled.button`
   padding: 8px 16px;
   border-radius: 2px;
-  width: 50px;
+  width: 30px;
   margin-left: 40%;
   background-color: #3f51b5;
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
@@ -21,4 +21,9 @@ export const ButtonLoadMore = styled.button`
   min-width: 180px;
   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+
+  &:hover {
+    background-color: darkgray;
+    color: black;
+  }
 `;
